dca2e10d90f7679c426ffff71e4daa8878c46416
Sander de Kroon <sander@dekroon.xyz>
2017-10-05 21:42:14 +0200

    Updated tests.

--- tests/PosttypeTest.php
+++ tests/PosttypeTest.php
@@ -3,6 +3,8 @@
 namespace Sanderdekroon\Parlant\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Sanderdekroon\Parlant\Posttype;
+use Sanderdekroon\Parlant\Builder\PosttypeBuilder;
 
 /**
  * Test the Posttype class
@@ -13,24 +15,32 @@ class PosttypeTest extends TestCase
     /** The any method should return an instance of the PosttypeBuilder */
     public function testAnyMethodReturnsBuilder()
     {
-        $this->markTestIncomplete('This test has not been implemented yet.');
+        $query = Posttype::any();
+        $this->assertInstanceOf(PosttypeBuilder::class, $query);
     }
 
     /** The type method should return an instance of the PosttypeBuilder */
     public function testTypeMethodReturnsBuilder()
     {
-        $this->markTestIncomplete('This test has not been implemented yet.');
+        $query = Posttype::type('posttype');
+        $this->assertInstanceOf(PosttypeBuilder::class, $query);
     }
 
     /** The any() method should return all available posts */
     public function testAnyMethodReturnsAllPosts()
     {
-        $this->markTestIncomplete('This test has not been implemented yet.');
+        $arguments = Posttype::any()->get();
+
+        $this->assertArrayHasKey('posts_per_page', $arguments);
+        $this->assertEquals(-1, $arguments['posts_per_page']);
     }
 
     /** The type method should set the post_type argument correctly */
     public function testTypeSetsPosttype()
     {
-        $this->markTestIncomplete('This test has not been implemented yet.');
+        $query = Posttype::type('something')->get();
+
+        $this->assertArrayHasKey('post_type', $query);
+        $this->assertEquals('something', $query['post_type']);
     }
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,1 +1,13 @@
 <?php
+
+require 'vendor/autoload.php';
+
+// Configure Parlant to only return the array of arguments.
+// Obviously this is only for testing purposes.
+$parlant = new \Sanderdekroon\Parlant\Posttype;
+$parlant->configure(
+    new \Sanderdekroon\Parlant\Configurator\PosttypeConfigurator([
+        'return'        => 'argument',
+        'post_status'   => 'any',
+    ])
+);
