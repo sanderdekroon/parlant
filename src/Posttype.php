c5efd8062e3fefd7d2fe38ae7789cde3432ab356
Sander de Kroon <sander@dekroon.xyz>
2017-10-10 21:49:28 +0200

    Changed inner workings of the Posttype class. It now actually refreshes the builder when a new query is created.

--- src/Posttype.php
+++ src/Posttype.php
@@ -7,20 +7,16 @@ use Sanderdekroon\Parlant\Builder\BuilderInterface;
 
 class Posttype
 {
-
+    /**
+     * The PosttypeBuilder of the current query.
+     * @var PosttypeBuilder
+     */
     protected static $builder;
 
     /**
-     * Set the Posttype builder.
-     * @param BuilderInterface|null $builder
-     */
-    public function __construct(BuilderInterface $builder = null)
-    {
-        self::$builder ?: (self::$builder = $builder ?: new PosttypeBuilder); //I'm sorry
-    }
-
-    /**
-     * Entry method (for now) which instantiates the builder and passes the type.
+     * Set the post type in the newly created builder and return the builder.
+     * Since this is the start of a new query, a new instance of the
+     * PosttypeBuilder is created and set as a static property.
      * @param  string $type
      * @return PosttypeBuilder
      */
@@ -29,23 +25,39 @@ class Posttype
         return (new self())->builder()->type($type);
     }
 
-
+    /**
+     * Return posts of any post type.
+     * @return PosttypeBuilder
+     */
     public static function any()
     {
         return self::type('any');
     }
 
     /**
-     * Return the saved instance of the builder.
+     * Find a post by it's id.
+     * @param  int $postId
+     * @return WP_Post
+     */
+    public static function find($postId)
+    {
+        return self::any()->where('p', (int)$postId)->first();
+    }
+
+    /**
+     * Create a new instance of the builder, set it as static property and return it.
+     * This makes sure that a new builder is created when a new query is created.
      * @return PosttypeBuilder
      */
     private function builder()
     {
+        self::$builder = new PosttypeBuilder;
         return self::$builder;
     }
 
     /**
-     * Direct every call to the PosttypeBuilder class
+     * Direct every call to the PosttypeBuilder class. It uses the static property
+     * so that any previous query argument is preserved and applied at the end.
      * @param  string $method
      * @param  array $paramaters
      * @return mixed
@@ -56,6 +68,6 @@ class Posttype
             return $this->$method(...$paramaters);
         }
 
-        return $this->builder()->$method(...$paramaters);
+        return self::$builder->$method(...$paramaters);
     }
 }
