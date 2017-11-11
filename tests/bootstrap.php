399cc2b04107933042fdc97b12d22c8b829f0c3f
Sander de Kroon <sander@dekroon.xyz>
2017-11-11 13:45:52 +0100

    Simplified global configuration. Updated for PHPUnit.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,10 +4,7 @@ require 'vendor/autoload.php';
 
 // Configure Parlant to only return the array of arguments.
 // Obviously this is only for testing purposes.
-$parlant = new \Sanderdekroon\Parlant\Posttype;
-$parlant->configure(
-    new \Sanderdekroon\Parlant\Configurator\PosttypeConfigurator([
-        'return'        => 'argument',
-        'post_status'   => 'any',
-    ])
-);
+\Sanderdekroon\Parlant\Configurator\ParlantConfigurator::global([
+    'return'        => 'argument',
+    'post_status'   => 'any',
+]);
