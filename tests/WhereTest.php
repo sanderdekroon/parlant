f912f2eb14ed3a1ddf10650065bc452cfc7a7308
Sander de Kroon <sander@dekroon.xyz>
2018-01-13 15:23:03 +0100

    Updated tests to reflect the support for arrays of where statements.

--- tests/WhereTest.php
+++ tests/WhereTest.php
@@ -41,19 +41,19 @@ class WhereTest extends TestCase
     {
         $this->markTestIncomplete('This test and method has not been implemented yet.');
 
-        // $query = Posttype::any()->where([
-        //     'foo'       => 'bar',
-        //     'meaning'   => 'of life',
-        //     'universe'  => 42,
-        // ])->get();
+        $query = Posttype::any()->where([
+            ['foo', 'bar'],
+            ['meaning', 'of life'],
+            ['universe', 42],
+        ])->get();
 
-        // $this->assertArrayHasKey('foo', $query);
-        // $this->assertArrayHasKey('meaning', $query);
-        // $this->assertArrayHasKey('universe', $query);
+        $this->assertArrayHasKey('foo', $query);
+        $this->assertArrayHasKey('meaning', $query);
+        $this->assertArrayHasKey('universe', $query);
 
-        // $this->assertTrue($query['foo'] == 'bar');
-        // $this->assertTrue($query['meaning'] == 'of life');
-        // $this->assertTrue($query['universe'] == 42);
+        $this->assertTrue($query['foo'] == 'bar');
+        $this->assertTrue($query['meaning'] == 'of life');
+        $this->assertTrue($query['universe'] == 42);
     }
 
 
