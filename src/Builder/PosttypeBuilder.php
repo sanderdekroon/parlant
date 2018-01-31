e71d932335ad048d2ce01c345eedeb0ffc50ecc4
Sander de Kroon <sander@dekroon.xyz>
2018-01-31 21:37:17 +0100

    Implemented the WhereClause class within the PosttypeBuilder.

--- src/Builder/PosttypeBuilder.php
+++ src/Builder/PosttypeBuilder.php
@@ -103,78 +103,22 @@ class PosttypeBuilder implements BuilderInterface
         return $this;
     }
 
-
+    /**
+     * Start a new Where clause. This simply adds the arguments to the outer array.
+     * @param  string|array $column
+     * @param  string|null $operator
+     * @param  string|null $value
+     * @return $this
+     */
     public function where($column, $operator = null, $value = null)
     {
-        // If the column is an array, we will assume it is an array of key-value pairs
-        // and can add them each as a where clause.
-        if (is_array($column)) {
-            return $this->addArrayOfWheres($column);
-        }
+        $clause = new WhereClause($this->getGrammar());
 
-        // Here we will make some assumptions about the operator. If only 2 values are
-        // passed to the method, we will assume that the operator is an equals sign
-        // and keep going. Otherwise, we'll require the operator to be passed in.
-        list($value, $operator) = $this->prepareValueAndOperator(
-            $value,
-            $operator,
-            (func_num_args() == 2 || is_null($value)) //Is this the best solution?
-        );
-
-        // If the given operator is not found in the list of valid operators we will
-        // assume that the developer is just short-cutting the '=' operators and
-        // we will set the operators to '=' and set the values appropriately.
-        if ($this->invalidOperator($operator)) {
-            list($value, $operator) = [$operator, '='];
-        }
-
-
-        // If the value is "null", we will just assume the developer wants to add a
-        // where null clause to the query. So, we will allow a short-cut here to
-        // that method for convenience so the developer doesn't have to check.
-        // if (is_null($value)) {
-        //     return $this->whereNull($column, $boolean, $operator != '=');
-        // }
-
-        // Now that we are working with just a simple query we can put the elements
-        // in our array and add the query binding to our array of bindings that
-        // will be bound to each SQL statements when it is finally executed.
-        $type = 'Basic';
-
-        $this->appendBinding('wheres', compact(
-            'type',
-            'column',
-            'operator',
-            'value'
-        ));
-
-        return $this;
-    }
-
-    /**
-     * Add an array of where statements. The array should contain a numeric array
-     * where the values can be used as arguments for the where() method.
-     * @param array $wheres
-     */
-    protected function addArrayOfWheres($wheres)
-    {
-        foreach ($wheres as $where) {
-            list($column, $operator, $value, $boolean) = $this->extractWhereValuesFromArray($where);
-            $this->where($column, $operator, $value, $boolean);
+        foreach ($clause->build($column, $operator, $value) as $where) {
+            $this->appendBinding('wheres', $where);
         }
 
         return $this;
-    }
-
-    
-    protected function extractWhereValuesFromArray($array)
-    {
-        return [
-            isset($array[0]) ? $array[0] : null,
-            isset($array[1]) ? $array[1] : null,
-            isset($array[2]) ? $array[2] : null,
-            isset($array[3]) ? $array[3] : null,
-        ];
     }
 
     /**
@@ -207,6 +151,7 @@ class PosttypeBuilder implements BuilderInterface
      * @param  boolean      $useDefault
      * @throws InvalidArgumentException
      * @return array
+     * @todo  make deprecated
      */
     protected function prepareValueAndOperator($value, $operator, $useDefault = false, $termDefault = false)
     {
@@ -245,6 +190,7 @@ class PosttypeBuilder implements BuilderInterface
      * Determine if an operator is invalid or unsupported
      * @param  string $operator
      * @return bool
+     * @todo  make deprecated
      */
     private function invalidOperator($operator)
     {
